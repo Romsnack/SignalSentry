e6e98884cce15919d3982fd088cf983b65ee34eb
romsnack <romsnack@snacks.none>
2024-02-13 23:55:02 +0000

    Added more packet information when capturing. Added a script to rapidly rebuild the project.

--- README.md
+++ README.md
@@ -6,5 +6,23 @@ The goal of this project is to understand how packet sniffers works and handling
 To build and use SignalSentry use the following:
 
 ```
-mkdir build && cd build && cmake .. && cmake --build . && cd .. && ./build/SignalSentry
+mkdir build && cd build && cmake .. && cmake --build . && cd .. && sudo ./build/SignalSentry
 ```
+
+You have to execute this program as root since it is using raw sockets to listen to the chosen network interface.
+
+The different services that are handled to this date are:
+
+    - SMTP
+    - SSH
+    - HTTP
+    - FTP
+    - DNS
+
+You can test each services on the loopback network interface (lo) by sending commands to 127.0.0.1. If you try to send packets to localhosts, adresses will be configured to be IPV6 which are not handled by SignalSentry. Here are a list of commands to test packet capturing on different services. Make sure you have the corresponding services up and running on their default ports:
+
+    - HTTP : curl http://localhost
+    - ssh : ssh 127.0.0.1
+    - ftp : ftp 127.0.0.1
+    - dns : dig @127.0.0.1
+    - SMTP : Use telnet to connect to port 25 (default SMTP port) then you can interact with your SMTP server and will get various informations about the captured packets.
--- main.cpp
+++ main.cpp
@@ -7,6 +7,73 @@
 #include <unistd.h>
 #include <net/if.h>
 #include <cstring>
+#include <netinet/ip.h>
+#include <iomanip>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+
+// Helper function to print known protocol details
+void printProtocolDetails(int sourcePort, int destPort, const char* payload, int payloadLength) {
+    // Identify protocol based on port numbers and payload signatures
+    if (sourcePort == 80 || destPort == 80 || (payloadLength > 0 && strncmp(payload, "HTTP", 4) == 0)) {
+        std::cout << "Protocol: HTTP" << std::endl;
+    } else if (sourcePort == 443 || destPort == 443) {
+        std::cout << "Protocol: HTTPS" << std::endl;
+    } else if (sourcePort == 21 || destPort == 21) {
+        std::cout << "Protocol: FTP Control" << std::endl;
+    } else if (sourcePort == 20 || destPort == 20) {
+        std::cout << "Protocol: FTP Data" << std::endl;
+    } else if (sourcePort == 25 || destPort == 25) {
+        std::cout << "Protocol: SMTP" << std::endl;
+    } else if (sourcePort == 53 || destPort == 53) {
+        std::cout << "Protocol: DNS" << std::endl;
+    } else if (sourcePort == 22 || destPort == 22) {
+        std::cout << "Protocol : SSH" << std::endl;
+    } else {
+        std::cout << "Protocol: Unknown" << std::endl;
+    }
+}
+
+void printPacketInfo(const char* buffer, int length) {
+    struct iphdr* ipHeader = (struct iphdr*)buffer;
+    int ipHeaderLength = ipHeader->ihl * 4;
+
+    // Check if the packet contains an IPv4 header
+    if (ipHeader->version == 4) {
+        std::cout << "Source IP: " << inet_ntoa(*(struct in_addr*)&ipHeader->saddr) << std::endl;
+        std::cout << "Destination IP: " << inet_ntoa(*(struct in_addr*)&ipHeader->daddr) << std::endl;
+
+        if (ipHeader->protocol == IPPROTO_TCP) {
+            struct tcphdr* tcpHeader = (struct tcphdr*)(buffer + ipHeaderLength);
+            int tcpHeaderLength = tcpHeader->doff * 4;
+
+            const char* payload = buffer + ipHeaderLength + tcpHeaderLength;
+            int payloadLength = length - ipHeaderLength - tcpHeaderLength;
+
+            int sourcePort = ntohs(tcpHeader->source);
+            int destPort = ntohs(tcpHeader->dest);
+
+            std::cout << "Source Port: " << sourcePort << std::endl;
+            std::cout << "Destination Port: " << destPort << std::endl;
+
+            printProtocolDetails(sourcePort, destPort, payload, payloadLength);
+        } else if (ipHeader->protocol == IPPROTO_UDP) {
+            struct udphdr* udpHeader = (struct udphdr*)(buffer + ipHeaderLength);
+            const char* payload = buffer + ipHeaderLength + sizeof(struct udphdr);
+            int payloadLength = length - ipHeaderLength - sizeof(struct udphdr);
+
+            int sourcePort = ntohs(udpHeader->source);
+            int destPort = ntohs(udpHeader->dest);
+
+            std::cout << "Source Port: " << sourcePort << std::endl;
+            std::cout << "Destination Port: " << destPort << std::endl;
+
+            printProtocolDetails(sourcePort, destPort, payload, payloadLength);
+        } else {
+            std::cout << "Protocol: " << static_cast<int>(ipHeader->protocol) << std::endl;
+        }
+    }
+}
 
 int main() {
     // Create a raw socket
@@ -41,7 +108,7 @@ int main() {
         }
 
         // Process the packet (buffer) here
-
+        printPacketInfo(buffer, bytesReceived);
         std::cout << "Packet received!" << std::endl;
     }
 
